871b9b9cc4574d81fea808197f56f4905cb5589c
LeoHuang <leohuang1996@gmail.com>
2017-10-17 17:28:31 +0800

    設定 '/' 的route name，對應到 HomeController

--- routes/web.php
+++ routes/web.php
@@ -10,10 +10,12 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+/*
 Route::get('/', function () {
     return view('welcome');
 });
+*/
+Route::get('/',['as' => 'home.index', 'uses' => 'HomeController@index']);
 
 Route::get('/about', function () {
     return view('welcome');
